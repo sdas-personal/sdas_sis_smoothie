c16fe604afb9b4c38b47b5db0729c6995b08dac0
sdas-personal <das.swarupkumar@gmail.com>
2026-03-23 13:47:43 -0700

    Create and update streamlit_app.py
    connected changes needed bc of move from SiS to SniS

---
+++ streamlit_app.py
@@ -0,0 +1,56 @@
+# Import python packages
+import streamlit as st
+# from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app HEADER
+st.title(f":cup_with_straw: Customize Your Smoothie ! :cup_with_straw:")
+st.write("""Choose the fruits you want in your custom Smoothie !""")
+
+#Adding a name box in UI to collect the name on the order
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on the Smoothie will be:", name_on_order)
+
+#Choose Option- Manual List
+#option = st.selectbox(
+#    "What is your favorite food?",
+#    ("Banana", "Strawberries", "Peaches"),
+#)
+#st.write("Your favorite fruit selected is:", option)
+
+
+
+# Choose options for order customization - From underlying table
+# session = get_active_session()
+cnx=st.connection("snowflake")
+session=cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_list = st.multiselect(
+    "Choose upto 5 ingredients :"
+    ,my_dataframe
+    ,max_selections=5
+)
+
+if ingredients_list:
+#    st.write("You Selected",ingredient_list)
+#    st.text(ingredient_list)
+
+    ingredients_string=''
+
+    for fruit_choosen in ingredients_list:
+        ingredients_string += fruit_choosen +' '
+
+    #st.write("You finalized fruit list : ",ingredients_string)
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                    values ('""" + ingredients_string + """','"""+ name_on_order +"""')"""
+
+    #st.write(my_insert_stmt)
+    time_to_insert= st.button('Submit Order')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success(f'Your Smoothie is ordered! {name_on_order}', icon="✅")
+
