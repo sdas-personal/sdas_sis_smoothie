9294e71d12387667d534a404cf84231681285a2c
sdas-personal <das.swarupkumar@gmail.com>
2026-03-26 17:35:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ if ingredients_list:
 
 # New Section to display smoothiefroot nutrition information
 
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response.json())
+#import requests  
+#smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+#st.text(smoothiefroot_response.json())
 
